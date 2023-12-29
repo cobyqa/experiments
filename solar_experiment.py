ff669f9917c1d1cb9943a1a604dbfe895da42de1
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-12-29 14:54:17 +0800

    Solve the problem with COBYQA

--- solar_experiment.py
+++ solar_experiment.py
@@ -4,6 +4,7 @@ import tempfile
 from pathlib import Path
 
 import numpy as np
+from cobyqa import minimize
 
 
 class SOLARProblem:
@@ -14,6 +15,14 @@ class SOLARProblem:
         if not isinstance(pb_id, int) or pb_id < 1 or pb_id > 10:
             raise ValueError('pb_id must be an integer between 1 and 10')
         self._pb_id = pb_id
+
+        self._path_tests = Path(__file__).parent / 'solar' / 'tests'
+        self._path_solar = Path(__file__).parent / 'solar' / 'bin' / 'solar'
+        if not self._path_tests.is_dir():
+            raise FileNotFoundError('solar tests directory not found')
+        if not self._path_solar.is_file():
+            raise FileNotFoundError('solar executable not found')
+
         self._history = {}
 
     @property
@@ -41,9 +50,8 @@ class SOLARProblem:
         pb_id = self._pb_id
         if pb_id == 10:
             pb_id = 6
-        path_tests = Path(__file__).parent / 'solar' / 'tests'
         try:
-            path_param = next(path_tests.glob(f'{pb_id}_*/param.txt'))
+            path_param = next(self._path_tests.glob(f'{pb_id}_*/param.txt'))
             with open(path_param) as f:
                 for line in f:
                     match = re.compile(fr'^{param}\s*\((?P<value>.*)\)$').match(line)
@@ -53,7 +61,7 @@ class SOLARProblem:
                             param_value = param_value.replace('-', str(fill_unknown))
                         return np.fromstring(param_value, sep=' ')
         except StopIteration:
-            path_x0 = next(path_tests.glob(f'{pb_id}_*/x0.txt'))
+            path_x0 = next(self._path_tests.glob(f'{pb_id}_*/x0.txt'))
             x0 = np.fromfile(path_x0, sep=' ')
             if param == 'X0':
                 return x0
@@ -71,14 +79,28 @@ class SOLARProblem:
         with tempfile.NamedTemporaryFile(delete_on_close=False) as f:
             f.write(str.encode(x_str))
             f.close()
-            path_solar = Path(__file__).parent / 'solar' / 'bin' / 'solar'
-            result = subprocess.run([path_solar, str(self._pb_id), f.name], stdout=subprocess.PIPE)
+            result = subprocess.run([self._path_solar, str(self._pb_id), f.name], stdout=subprocess.PIPE)
         output = np.fromstring(result.stdout.decode(), sep=' ')
         self._history[x_str] = (output[0], output[1:])
         return output
 
 
 if __name__ == '__main__':
-    pb_id = 1
-    p = SOLARProblem(pb_id)
-    print(p.fun(p.x0))
+    for pb_id in [6, 10]:
+        print(f'Solving SOLAR{pb_id}')
+        print()
+
+        p = SOLARProblem(pb_id)
+        options = {
+            'verbose': True,
+            'store_history': True,
+        }
+        res = minimize(p.fun, p.x0, xl=p.xl, xu=p.xu, cub=p.cub, options=options)
+
+        output = Path(__file__).parent / 'out' / f'solar{pb_id}'
+        output.mkdir(parents=True, exist_ok=True)
+        np.save(output / 'fun_history.npy', res.fun_history)
+        np.save(output / 'cub_history.npy', res.cub_history)
+        print()
+        print(f'Results saved in {output}')
+        print()
