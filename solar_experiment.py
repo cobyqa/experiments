38222024843bc011ca6c740a436a28227065bbef
Tom M. Ragonneau <t.ragonneau@gmail.com>
2024-01-04 20:52:40 +0800

    Improve SOLARProblem class

--- solar_experiment.py
+++ solar_experiment.py
@@ -1,12 +1,23 @@
 import re
 import subprocess
 import tempfile
+from enum import Enum
 from itertools import product
 from pathlib import Path
 
 import numpy as np
 from cobyqa import minimize
 from joblib import Parallel, delayed
+
+
+class SOLARInputType(str, Enum):
+    REAL = 'R'
+    INTEGER = 'I'
+
+
+class SOLAROutputType(str, Enum):
+    OBJECTIVE = 'OBJ'
+    CONSTRAINT = 'CSTR'
 
 
 class SOLARProblem:
@@ -18,67 +29,75 @@ class SOLARProblem:
             raise ValueError('pb_id must be an integer between 1 and 10')
         self._pb_id = pb_id
 
-        self._path_tests = Path(__file__).parent / 'solar' / 'tests'
         self._path_solar = Path(__file__).parent / 'solar' / 'bin' / 'solar'
-        if not self._path_tests.is_dir():
-            raise FileNotFoundError('solar tests directory not found')
         if not self._path_solar.is_file():
             raise FileNotFoundError('solar executable not found')
+
+        self._raw_params = self._get_raw_params()
 
         self._history = {}
 
     @property
     def n(self):
-        return self.x0.size
+        return int(self._raw_params['DIMENSION'])
 
     @property
     def x0(self):
-        return self._get_param('X0')
+        return self._get_array('X0')
 
     @property
     def xl(self):
-        return self._get_param('LOWER_BOUND', -np.inf)
+        return self._get_array('LOWER_BOUND')
 
     @property
     def xu(self):
-        return self._get_param('UPPER_BOUND', np.inf)
+        return self._get_array('UPPER_BOUND')
 
     def fun(self, x):
         try:
-            return self._eval(x)[0]
+            obj = self._eval(x)[0]
+            if obj.size == 1:
+                return obj[0]
+            return obj
         except:
             return np.inf
 
     def cub(self, x):
-        return self._eval(x)[1]
-
-    def _get_param(self, param, fill_unknown=None):
-        pb_id = self._pb_id
-        if pb_id == 10:
-            pb_id = 6
         try:
-            path_param = next(self._path_tests.glob(f'{pb_id}_*/param.txt'))
-            with open(path_param) as f:
-                for line in f:
-                    match = re.compile(fr'^{param}\s*\((?P<value>.*)\)$').match(line)
-                    if match:
-                        param_value = match.group('value')
-                        if fill_unknown is not None:
-                            param_value = param_value.replace('-', str(fill_unknown))
-                        return np.fromstring(param_value, sep=' ')
-        except StopIteration:
-            path_x0 = next(self._path_tests.glob(f'{pb_id}_*/x0.txt'))
-            x0 = np.fromfile(path_x0, sep=' ')
-            if param == 'X0':
-                return x0
-            elif param == 'LOWER_BOUND':
-                return np.full(x0.size, -np.inf)
-            elif param == 'UPPER_BOUND':
-                return np.full(x0.size, np.inf)
-        raise ValueError(f'parameter {param} not found')
+            return self._eval(x)[1]
+        except:
+            output_types = self._raw_params['BB_OUTPUT_TYPE'].split()
+            return np.full(len([output_type for output_type in output_types if output_type == SOLAROutputType.CONSTRAINT]), np.inf)
+
+    def _get_raw_params(self):
+        result = subprocess.run([self._path_solar, str(self._pb_id)], stdout=subprocess.PIPE)
+        output = result.stdout.decode()
+        lines = output.splitlines()
+        raw_params = {}
+        for line in lines[lines.index('NOMAD parameters:') + 2:]:
+            prog = re.compile(r'^\s*(?P<param>[A-Z0-9_]+)\s*(?P<value>.*)$')
+            match = prog.match(line)
+            if match:
+                raw_params[match.group('param')] = match.group('value')
+        return raw_params
+
+    def _get_array(self, param):
+        input_types = self._raw_params['BB_INPUT_TYPE'][1:-1].split()
+        array = {}
+        raw_array = self._raw_params[param][1:-1].split()
+        for i in range(self.n):
+            if raw_array[i] == '-':
+                array[f'x{i + 1}'] = None
+            elif input_types[i] == SOLARInputType.REAL:
+                array[f'x{i + 1}'] = float(raw_array[i])
+            elif input_types[i] == SOLARInputType.INTEGER:
+                array[f'x{i + 1}'] = int(raw_array[i])
+            else:
+                raise ValueError(f'Unknown input type: {input_types[i]}')
+        return array
 
     def _eval(self, x):
-        x_str = ' '.join(map(str, x))
+        x_str = ' '.join(map(str, x.values()))
         if x_str in self._history:
             return self._history[x_str]
 
@@ -87,8 +106,12 @@ class SOLARProblem:
             f.close()
             result = subprocess.run([self._path_solar, str(self._pb_id), f.name], stdout=subprocess.PIPE)
         output = np.fromstring(result.stdout.decode(), sep=' ')
-        self._history[x_str] = (output[0], output[1:])
-        return output
+        output_types = self._raw_params['BB_OUTPUT_TYPE'].split()
+        self._history[x_str] = (
+            output[[j for j, output_type in enumerate(output_types) if output_type == SOLAROutputType.OBJECTIVE]],
+            output[[j for j, output_type in enumerate(output_types) if output_type == SOLAROutputType.CONSTRAINT]],
+        )
+        return self._history[x_str]
 
 
 @delayed
@@ -108,17 +131,21 @@ def solve(pb_id, i_restart):
 
 
 if __name__ == '__main__':
-    n_restart = 10
-    # Parallel(n_jobs=-1)(solve(pb_id, i_restart) for pb_id, i_restart in product([6, 10], range(n_restart)))
-    for pb_id in [6, 10]:
-        for i_restart in range(n_restart):
-            try:
-                output = Path(__file__).parent / 'out' / f'solar{pb_id}_{i_restart}'
-                fun_history = np.load(output / 'fun_history.npy')
-                cub_history = np.load(output / 'cub_history.npy')
-                if cub_history.size > 0:
-                    maxcv_history = np.max(cub_history, 1, initial=0.0)
-                    fun_history = fun_history[maxcv_history <= 0.0]
-                print(f'SOLAR{pb_id}({i_restart}): {np.min(fun_history)}')
-            except FileNotFoundError:
-                print(f'SOLAR{pb_id}({i_restart}): not solved')
+    p = SOLARProblem(1)
+    print(p.fun(p.x0))
+    print(p.cub(p.x0))
+    print(p.fun(p.x0))
+    # n_restart = 10
+    # # Parallel(n_jobs=-1)(solve(pb_id, i_restart) for pb_id, i_restart in product([6, 10], range(n_restart)))
+    # for pb_id in [6, 10]:
+    #     for i_restart in range(n_restart):
+    #         try:
+    #             output = Path(__file__).parent / 'out' / f'solar{pb_id}_{i_restart}'
+    #             fun_history = np.load(output / 'fun_history.npy')
+    #             cub_history = np.load(output / 'cub_history.npy')
+    #             if cub_history.size > 0:
+    #                 maxcv_history = np.max(cub_history, 1, initial=0.0)
+    #                 fun_history = fun_history[maxcv_history <= 0.0]
+    #             print(f'SOLAR{pb_id}({i_restart}): {np.min(fun_history)}')
+    #         except FileNotFoundError:
+    #             print(f'SOLAR{pb_id}({i_restart}): not solved')
